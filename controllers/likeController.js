88d13abe571dabcb20356088cf877dc240efb1c8
mhd-atheel <aathilmazz1234@gmail.com>
2023-08-25 20:24:53 +0530

    add delete like controller and route

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -5,12 +5,20 @@ const createLike = async (req, res) => {
   try {
     const { userid, postid } = req.body;
 
+    const existingLike = await Like.findOne({ userid, postid });
+
+    if (existingLike) {
+      return res.status(400).json({ message: "User has already liked this post." });
+    }
+
     const newLike = new Like({
       userid,
       postid,
     });
     const like = await newLike.save();
     const foundPost = await Post.findOne({ _id: postid });
+
+  
     const likeCounts = await foundPost.likecount;
     //console.log("Like count:", likeCount)
     await Post.findByIdAndUpdate(
@@ -25,6 +33,29 @@ const createLike = async (req, res) => {
   }
 };
 
+const deletelike = async (req,res) => {
+  const { userid, postid } = req.body;
+  try {
+    // Find the like with the specified userid and postid
+    const likeToDelete = await Like.findOne({ userid, postid });
+
+    if (!likeToDelete) {
+      return res.status(404).json({ message: "Like not found." });
+    }
+
+    // Delete the found like
+    await likeToDelete.remove();
+
+    return res.status(200).json({ message: "Like deleted successfully." });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error." });
+  }
+}
+
+
+
+
 const getLikeById = async (req, res) => {
   try {
     const id = req.params.id;
@@ -38,4 +69,5 @@ const getLikeById = async (req, res) => {
 module.exports = {
   createLike,
   getLikeById,
+  deletelike
 };
--- routes/likeRoute.js
+++ routes/likeRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const {createLike} = require('../controllers/likeController')
+const {createLike, deletelike} = require('../controllers/likeController')
 const router = express.Router()
 
 
 router.post('/create-like',createLike),
+router.post('/delete-like',deletelike),
 
 
 
