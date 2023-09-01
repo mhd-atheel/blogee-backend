694f144b26c8d853d7e672d015dc1a2d94317904
mhd-atheel <aathilmazz1234@gmail.com>
2023-09-01 23:42:16 +0530

    add socket.io create socket connetion

--- index.js
+++ index.js
@@ -1,27 +1,56 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const  mongoose  = require('mongoose')
-const userRouter= require('./routes/userRouter.js')
-const postRouter= require('./routes/postRoute.js')
-const LikeRouter= require('./routes/likeRoute.js')
-const CommentRouter= require('./routes/commentRoute.js')
-const SavedRouter= require('./routes/savePostRouter.js')
-const cors = require('cors')
+const express = require('express');
+const dotenv = require('dotenv');
+const socketIo = require('socket.io');
+const http = require('http');
+const  mongoose  = require('mongoose');
+const userRouter= require('./routes/userRouter.js');
+const postRouter= require('./routes/postRoute.js');
+const LikeRouter= require('./routes/likeRoute.js');
+const CommentRouter= require('./routes/commentRoute.js');
+const SavedRouter= require('./routes/savePostRouter.js');
+const cors = require('cors');
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-dotenv.config()
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+app.use(cors());
+app.use(express.json());
+dotenv.config();
 
 const port = process.env.PORT
 const url = process.env.MONGO_URI
 
+const Message = mongoose.model('Message', {
+    text: String,
+    user: String,
+  });
 
-app.listen(port ,()=>{
+  io.on('connection', (socket) => {
+    console.log('A user connected');
+    console.log(socket.id);
+    // // Handle custom events
+    // socket.on('chat message', (message) => {
+    //   console.log('Received message:', message);
+  
+    //   // Save the message to MongoDB
+    //   const newMessage = new Message(message);
+    //   newMessage.save().then(() => {
+    //     // Broadcast the message to all connected clients
+    //     io.emit('chat message', message);
+    //   });
+    // });
+  
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
+    });
+  });
+
+server.listen(port,'0.0.0.0' ,()=>{
 
     console.log(`Successfully connected ${port}`);
 })
 
+mongoose.set('strictQuery', false);
 mongoose.connect(url,{
     useNewUrlParser: true,
     useUnifiedTopology: true
